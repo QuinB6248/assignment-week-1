ece7b4a8d7f0470642e007ff93ebae5e7501627e
QuinB6248 <4_sAfJO25N@outlook.com>
2019-04-26 22:47:03 +0200

    first draft: all boxes checked on run test

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,1 +1,37 @@
 // Write your JS here
+const hero = {
+    name: "",
+    heroic: true,
+    inventory: [],
+    health: 10,
+    weapon: {
+        type: "",
+        damage: 2
+    }
+};
+
+function rest(object) {
+    if (object.health === 10) {
+        alert("You are topfit!")
+    } else {
+        object.health = 10
+    };
+     
+     return object
+};
+
+function pickUpItem(hero, weapon) {
+    weapon.type = "dagger";
+    weapon.damage = 2;
+    hero.inventory.push(weapon);
+
+};
+
+function equipWeapon(hero) {
+    if (hero.inventory.length === 0) {
+        return
+    } else {
+        hero.weapon = hero.inventory[0];
+    }
+    
+};
