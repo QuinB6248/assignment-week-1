939fcde504695ca8922f6066e99a1f5acd2b2752
QuinB6248 <4_sAfJO25N@outlook.com>
2019-04-27 19:19:36 +0200

    made a displayStats function

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -2,17 +2,12 @@
 
 
 
-function submitName() {
-    const nameInput = document.getElementById("hero_name");
-    const name = nameInput.value;
-    document.getElementById("change_name").textContent = name.toUpperCase();
-    nameInput.value = null;
-}
+
 
 
 
 const hero = {
-    name: "",
+    name: "hero",
     heroic: true,
     inventory: [],
     health: 10,
@@ -22,28 +17,52 @@ const hero = {
     }
 };
 
+// change the name of HERO into submitted name, store that name in object hero
+function submitName() {
+    const nameInput = document.getElementById("hero_name");
+    const nameHero = nameInput.value;
+    document.getElementById("change_name").textContent = nameHero.toUpperCase();
+    hero.name = nameHero;
+    nameInput.value = null;
+}
+
+// restore health to 10 inside hero object
 function rest(object) {
     if (object.health === 10) {
         alert("You are topfit!")
     } else {
         object.health = 10
     };
-     
+    displayStats (hero);
      return object
 };
 
+// change weapon type to dagger and damage to 2, push this object in invemntory array of hero object
 function pickUpItem(hero, weapon) {
     weapon.type = "dagger";
     weapon.damage = 2;
     hero.inventory.push(weapon);
+    
+    displayStats (hero);
+    
 
 };
 
+// weapon becomes the first item of the inventory array of hero object
 function equipWeapon(hero) {
     if (hero.inventory.length === 0) {
         return
     } else {
         hero.weapon = hero.inventory[0];
     }
-    
+    displayStats (hero);
 };
+
+function displayStats (hero) {
+    
+    document.getElementById("stats_name").textContent = hero.name;
+    document.getElementById("stats_health").textContent = hero.health;
+    document.getElementById("stats_type").textContent = hero.weapon.type;
+    document.getElementById("stats_damage").textContent = hero.weapon.damage;
+
+}
